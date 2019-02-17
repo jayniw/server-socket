f840212e19fd13337b8fa4daabeefe2b024eef6c
Jalir Duran <jayniw@gmail.com>
2019-02-16 21:37:03 -0400

    configurado los mensajes privados y globales

--- routes/router.ts
+++ routes/router.ts
@@ -1,4 +1,5 @@
 import {Router,Response,Request} from 'express';
+import Server from '../classes/server';
 
 //definir router para los endpoints
 const router = Router();
@@ -16,6 +17,15 @@ router.post('/mensajes',(req:Request,res:Response)=>{
   //obtener lo enviado en el POST del endpoint
   const cuerpo = req.body.cuerpo;
   const de = req.body.de;
+  //payload del mensaje
+  const payload = {
+    de,
+    cuerpo
+  };
+  //instanciar el server
+  const server = Server.instance;
+  //enviar el mansaje a los conectados
+  server.io.emit('mensaje-nuevo', payload);
   res.json({
     ok:true,
     cuerpo,
@@ -28,6 +38,16 @@ router.post('/mensajes/:id',(req:Request,res:Response)=>{
   const cuerpo = req.body.cuerpo;
   const de = req.body.de;
   const id = req.params.id;
+  //payload del mensaje
+  const payload = {
+    de,
+    cuerpo
+  };
+  //instanciar el server
+  const server = Server.instance;
+  //enviar el mensaje al usuario id
+  server.io.in(id).emit('mensaje-privado', payload);
+
   res.json({
     ok:true,
     cuerpo,
